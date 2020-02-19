60289020ec467c8e3efa75bde34b60bfb596fa63
Gray, John F <John.Gray@calpers.ca.gov>
2020-02-19 14:16:47 -0800

    updates

--- compass-project/schedule-monitor/src/main/java/compass/schedule/monitor/HeartbeatJob.java
+++ compass-project/schedule-monitor/src/main/java/compass/schedule/monitor/HeartbeatJob.java
@@ -4,25 +4,29 @@ import org.quartz.JobDataMap;
 import org.quartz.JobDetail;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.springframework.http.HttpMethod;
 import org.springframework.scheduling.quartz.QuartzJobBean;
-
+import org.springframework.web.reactive.function.client.WebClient;
 import org.slf4j.Logger; import org.slf4j.LoggerFactory;
 
 public class HeartbeatJob extends QuartzTriggerJob {
 	private static Logger log = LoggerFactory.getLogger(HeartbeatJob.class);
 
-	
 //	@Override
 	protected void executeInternal(JobExecutionContext jobContext) throws JobExecutionException {
 		setJobDetail(jobContext.getJobDetail());
 		fireInstanceId = jobContext.getFireInstanceId();
-		
+		StringBuilder sb = new StringBuilder("♥♥♥♥♥ ");
+		sb.append(fireInstanceId);
 		JobDetail jobDetail = jobContext.getJobDetail();
 		JobDataMap jobDataMap = jobContext.getMergedJobDataMap();
-		for(var key: jobDataMap.getKeys()) {
-			log.info(String.format("%s: %s", key, jobDataMap.getString(key)));
+		String filename = jobDataMap.getString("filename");
+		if(jobDataMap.getBooleanFromString("checkFileMonitor").booleanValue()) {
+			WebClient client = WebClient.create(jobDataMap.getString("fileMonitorUrl"));
+			WebClient.RequestBodySpec uri = client.method(HttpMethod.GET);
+			
 		}
-		log.info(String.format("Heartbeat %s %s fired at %s will fire again at %tc", fireInstanceId, jobDetail.getKey(), jobContext.getFireTime(), jobContext.getNextFireTime()));
+		log.info(String.format("♥♥♥♥♥ %s", fireInstanceId));
 	}
 
 }
